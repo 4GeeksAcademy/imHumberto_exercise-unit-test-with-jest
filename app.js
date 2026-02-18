02972ba7324aa7c88f5fdfa444a0ab72e9798a70
Humberto Sánchez <30481822+imHumberto@users.noreply.github.com>
2026-02-18 21:32:40 +0000

    test jest

---
+++ app.js
@@ -0,0 +1,31 @@
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+// De Dollar a Yen
+function fromDollarToYen(dollars) {
+    let euros = dollars / oneEuroIs.USD;
+    let yen = euros * oneEuroIs.JPY;
+    return yen;
+}
+
+// De Euro a Dollar
+function fromEuroToDollar(euros) {
+    let dollars = euros * oneEuroIs.USD;
+    return dollars;
+}
+
+// De Yen a Pound
+function fromYenToPound(yen) {
+    let euros = yen / oneEuroIs.JPY;
+    let pounds = euros * oneEuroIs.GBP;
+    return pounds;
+}
+
+console.log(fromDollarToYen(1))
+console.log(fromEuroToDollar(1))
+console.log(fromYenToPound(1))
+
+module.exports = {fromEuroToDollar, fromEuroToDollar, fromYenToPound}
